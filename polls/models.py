914cf6d88f8977541d5251c41c2828bf3d038006
Benjamin <benjaminleonlind@gmail.com>
2019-01-27 11:06:34 +0100

    Test and fix the issue that questions with future time stamps should not be refered to as recent questions.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,6 +11,11 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        yesterday = now - datetime.timedelta(days=1)
+        return yesterday <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
