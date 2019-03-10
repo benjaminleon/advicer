819b7ba51ccbb184c71b4a4937458e5b3c787690
Benjamin <benjaminleonlind@gmail.com>
2019-03-10 18:33:58 +0100

    Implement login by following the library tutorial

--- soulfilm/settings.py
+++ soulfilm/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/polls/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
