5a054aa45b5877ee62b6ebd5b9f25aa0dc3470e5
Benjamin <benjaminleonlind@gmail.com>
2019-05-23 16:03:24 +0200

    Add user signup functionality

--- soulfilm/urls.py
+++ soulfilm/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('tips/', include('tips.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
