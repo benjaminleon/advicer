0b54de150daac0227a542a20760eed6a0ccc3aa8
Benjamin <benjaminleonlind@gmail.com>
2019-06-15 15:56:01 +0200

    Define static_root in settings.py and make static/ directory

--- soulfilm/settings.py
+++ soulfilm/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 AUTH_USER_MODEL = 'tips.CustomUser'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
