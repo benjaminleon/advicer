cdec370c73485d6eca4987f8d895d67f47309f30
Benjamin <benjaminleonlind@gmail.com>
2019-07-12 22:54:33 +0200

    Continue implementation of login_required

--- tips/urls.py
+++ tips/urls.py
@@ -4,12 +4,10 @@ from django.contrib.auth.decorators import login_required
 
 app_name = 'tips'  # Namespacing
 urlpatterns = [
-    path('', login_required(views.index), name='index'),
+    path('', login_required(views.index, redirect_field_name=None), name='index'),
     path('rate/<int:movie_id>', views.NewRating, name='new_rating'),
     path('updateRating/<int:rating_id>', views.UpdateRating, name='update_rating'),
     path('deleteRating/<int:rating_id>', views.DeleteRating, name='delete'),
     path('deleteRatingByMovie/<int:movie_id>', views.DeleteRatingByMovie, name='deleteByMovie'),
-    path('search/', login_required(views.SearchResultsView.as_view(),
-     redirect_field_name=None),  # Skip next/blah
-      name='search_results'),
+    path('search/', login_required(views.SearchResultsView.as_view(), redirect_field_name=None), name='search_results'),
 ]
