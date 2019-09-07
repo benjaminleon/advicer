bf6a26c832a28e648a359ef4094c2cbaf806e314
Benjamin <benjaminleonlind@gmail.com>
2019-09-07 13:03:36 +0200

    Use f-strings in debug prints

--- tips/getRecommendations.py
+++ tips/getRecommendations.py
@@ -25,23 +25,23 @@ def getMostSimilarUsers(current_user_name, users_and_ratings):
         # Find the movies this user has in common with other users
         seen_movies = users_and_ratings[current_user_name]
         for seen_movie in seen_movies:
-            debugPrint("searching for {}".format(str(seen_movie)))
+            debugPrint(f'searching for {seen_movie}')
             score1 = users_and_ratings[current_user_name][seen_movie]
             if seen_movie in users_and_ratings[other_username]:
-                debugPrint("bingo for " + str(seen_movie))
+                debugPrint(f'bingo for {seen_movie}')
                 score2 = users_and_ratings[other_username][seen_movie]
                 similarity_scores[other_username] += abs(score1 - score2)
             else:
-                debugPrint("miss for " + str(seen_movie))
+                debugPrint(f'miss for {seen_movie}')
                 similarity_scores[other_username] += abs(score1 - UNSEEN_MOVIE_SCORE) + NO_MATCH_PUNISH
 
-            debugPrint("similarity_scores: {}".format(similarity_scores))
+            debugPrint(f'similarity scores: {similarity_scores}')
 
     most_similar_users = []
     while similarity_scores:
         most_similar_user = min(similarity_scores, key=similarity_scores.get)
         most_similar_users.append(most_similar_user)
-        debugPrint("The most similar users are {}".format(most_similar_users))
+        debugPrint(f'The most similar users are {most_similar_users}')
         similarity_scores.pop(most_similar_user)
 
     return most_similar_users
@@ -51,36 +51,36 @@ def getUnseenTopMoviesFrom(most_similar_users, users_and_ratings, current_user_n
     for user in most_similar_users:
         topMovies = getTopMoviesFrom(user, users_and_ratings)
         if topMovies:
-            debugPrint("Top movies from {}: {}".format(user, topMovies))
+            debugPrint(f'Top movies from {user}: {topMovies}')
             unseenTopMovies = []
             seen_movies = users_and_ratings[current_user_name]
-            debugPrint("Seen movies" + str(seen_movies))
+            debugPrint(f'Seen movies {seen_movies}')
             for movie in topMovies:
                 if movie not in seen_movies:
                     unseenTopMovies.append(movie)
 
-            debugPrint("Unseen top movies: {}".format(unseenTopMovies))
+            debugPrint(f'Unseen top movies: {unseenTopMovies}')
             if unseenTopMovies:
                 return unseenTopMovies
 
         else:
-            debugPrint("{} didn't have any movies with high enough rating".format(user))
+            debugPrint(f'{user} didn\'t have any movies with high enough rating')
 
     return ["Rate more movies and ask your friends to do the same to get recommendations :)"]
 
 
 def getTopMoviesFrom(user, users_and_ratings):
     if not user in users_and_ratings:
-        return ["{} does not exist".format(user)]
+        return [f'{user} does not exist']
 
-    debugPrint("looking for top movies from {}".format(user))
+    debugPrint(f'looking for top movies from {user}')
     ratings = users_and_ratings[user]
     topMovies = []
     good_score = MAX_SCORE
     while not topMovies and good_score > 2:
-        debugPrint("good score is {}".format(good_score))
+        debugPrint(f'good score is {good_score}')
         topMovies = [title for title in ratings if ratings[title] == good_score]
-        debugPrint("Top movies: {}".format(topMovies))
+        debugPrint(f'Top movies: {topMovies}')
         good_score = good_score - 1
 
     return topMovies
