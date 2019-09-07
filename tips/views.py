cf2532d62f93b61e71fc588084c7da0c7f7666e0
Benjamin <benjaminleonlind@gmail.com>
2019-09-07 14:41:32 +0200

    Search primarily for exact matches of words, and secondarily more loosely

--- tips/views.py
+++ tips/views.py
@@ -140,7 +140,9 @@ class SearchResultsView(generic.ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        matched_movies = Movie.objects.filter(Q(title__icontains=query))
+        matched_movies = Movie.objects.filter(Q(title__in=[query]))
+        if not matched_movies:
+            matched_movies = Movie.objects.filter(Q(title__icontains=query))
         matched_movies = matched_movies[:MAX_NR_OF_RESULTS]
 
         users_ratings = Rating.objects.filter(user=self.request.user)
